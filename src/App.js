a1b010869313e60d0523458d5ee2fbad4e3dd818
Luciano Vallejo <luciano.m.vallejo@gmail.com>
2022-03-15 01:29:40 -0300

    Agregado de logo y navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import "./App.css";
-import Button from "react-bootstrap/Button";
-import "bootstrap/dist/css/bootstrap.min.css";
-import clavedeDo from "./assets/clavedeDo.png";
+import NavBar from "./components/navbar-comp/NavBar.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <img src={clavedeDo} alt="" height="80px" width="80px"></img>
-          <p>MusicAll store</p>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Know us
-        </a>
-        <Button>bootstrap</Button>
+      <header>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/navbar-comp/NavBar.js
@@ -0,0 +1,52 @@
+import Button from "@mui/material/Button";
+import clavedeDo from "../../assets/img/clavedeDo.png";
+import Stack from "@mui/material/Stack";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div>
+        <Stack spacing={6} direction="row">
+          <img
+            src={clavedeDo}
+            className="img-header"
+            alt="none"
+            width={60}
+            height={60}
+          />
+          <Button variant="contained" size="small" className="button">
+            Home
+          </Button>
+          <Button variant="contained" size="small" className="button">
+            Productos
+          </Button>
+          <Button variant="contained" size="small" className="button">
+            Nosotros
+          </Button>
+          <Button variant="contained" size="small" className="button">
+            Contactos
+          </Button>
+        </Stack>
+        {/* <div>
+          <ul>
+            <li>
+              <Button variant="home">Home</Button>
+            </li>
+            <li>
+              <Button variant="productos">Productos</Button>
+            </li>
+            <li>
+              <Button variant="nosotros">Nosotros</Button>
+            </li>
+            <li>
+              <Button variant="contactos">Contactos</Button>
+            </li>
+          </ul>
+        </div> */}
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
